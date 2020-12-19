25bf7654645591a17f3a2dcba1a08d87ac30edfb
Vasyl Semak <vasylsmk1401@gmail.com>
2020-12-18 19:02:11 -0500

    complete: 'PetPreview' component

--- src/components/PetPreview.js
+++ src/components/PetPreview.js
@@ -2,22 +2,43 @@ import React, { Component } from 'react';
 import AdoptionForm from './AdoptionForm';
 
 export default class PetPreview extends Component {
-
   constructor () {
-
     super();
+    this.state = {
+      petToPreview: {}
+    };
 
+    this.previewPet = this.previewPet.bind(this);
+    this.adoptSelectedPet = this.adoptSelectedPet.bind(this);
+  }
+
+  previewPet(event) {
+    const { pets } = this.props;
+    const selectedPet = pets.find(p => p.name === event.target.value);
+    this.setState({ petToPreview: selectedPet });
+  }
+
+  adoptSelectedPet() {
+    const { petToPreview } = this.state;
+    const { adoptPet } = this.props;
+    adoptPet(petToPreview);
   }
 
   render () {
+    const imgUrl = this.state.petToPreview.imgUrl;
+    const pets = this.props.pets;
 
     return (
       <div className="preview">
         <div>
           <h5>Preview:</h5>
-          <img src={'TODO'} />
+          <img src={imgUrl} />
         </div>
-
+        <AdoptionForm
+          pets={pets}
+          previewPet={this.previewPet}
+          adoptSelectedPet={this.adoptSelectedPet}
+        />
       </div>
     );
 
